276106d8055c3ff84edeeea99be737104917015d
OpenShift guest <dukeastar@gmail.com>
2013-10-20 18:00:27 -0400

    Format and add argument to function

--- media/js/jquery.dataTables.listnav.2.js
+++ media/js/jquery.dataTables.listnav.2.js
@@ -13,7 +13,7 @@
             sLetterDisabledClass : "btn abcdaire disabled"
         };
 
-         $.extend(defaults,oDTSettings.oInit.oListNav);
+        $.extend(defaults,oDTSettings.oInit.oListNav);
 
 		var me = this;
 		var oDTTable = oDTSettings.oInstance;
@@ -28,7 +28,7 @@
 		var charCodeRange = {
 			start: 65,
 			end: 90
-		}
+		};
 
 		me.$container.append('<button data-letter="all" class="' + defaults.sLetterClass + '">'+ defaults.sAllWord +'</button>');
 		me.$container.append('<button data-letter="_" class="' + defaults.sLetterDisabledClass + '">0-9</button>');
@@ -40,80 +40,77 @@
 
 		oDTSettings.aoInitComplete.push( {
         
-	        "fn": function () {
-
-                if(oDTSettings.oFeatures.bServerSide == false)
+            "fn": function () {
+                if(oDTSettings.oFeatures.bServerSide === false)
                 {
                     var anNodes = oDTTable.oApi._fnGetTrNodes( oDTSettings );
-	            	$("td:nth-child("+(defaults.iIndex+1) + ")",anNodes).each(function(){
-	            		var l =$(this).text()[0].toUpperCase();
-	            		if(isNaN(l))
-	            			$('button[data-letter='+l+']',me.$container).removeClass('disabled');	            			
-	            		else
-	            			$('button[data-letter="_"]',me.$container).removeClass('disabled');
-	            	});
+                    $("td:nth-child("+(defaults.iIndex+1) + ")",anNodes).each(function(){
+                        var l =$(this).text()[0].toUpperCase();
+                        if(isNaN(l))
+                            $('button[data-letter='+l+']',me.$container).removeClass('disabled');
+                        else
+                            $('button[data-letter="_"]',me.$container).removeClass('disabled');
+                    });
                 }
                 else if(typeof defaults.fRetreiveFirstLetters == "function" )
-	        	{
-	        		$.each(defaults.fRetreiveFirstLetters(),function(){
-	        			var l =this[0].toUpperCase();
-	        			if(isNaN(l))
-	        				$('button[data-letter="'+l+'"]',me.$container).removeClass('disabled');
-	        			else
-	        				$('button[data-letter="_"]',me.$container).removeClass('disabled');
-	        		});
-	        	}
-	        },
-	        "sName": "ListNav"
+                {
+                    $.each(defaults.fRetreiveFirstLetters(oDTSettings),function(){
+                        var l =this[0].toUpperCase();
+                        if(isNaN(l))
+                            $('button[data-letter="'+l+'"]',me.$container).removeClass('disabled');
+                        else
+                            $('button[data-letter="_"]',me.$container).removeClass('disabled');
+                    });
+                }
+            },
+            "sName": "ListNav"
         } );
 
         $('button',me.$container).bind('click.DT',function(e){
-        	var me = $(this);
+            var me = $(this);
 			me.addClass("active").siblings().removeClass("active");
 			var letter = $(this).data('letter');
-       	    	
-			switch (letter)
-	    	{
-	    		case "all":
-	    			oDTTable.fnFilter("",defaults.iIndex,true,false); 
-	    			break;
-	    		case "_":
-	    			oDTTable.fnFilter("^[0-9]",defaults.iIndex,true,false); 
-	    			break;
-	    		default:
-	    			oDTTable.fnFilter("^"+letter,defaults.iIndex,true,false); 
-	    	}
-	    	return false;
-	     });
 
-	}
+			switch (letter)
+            {
+                case "all":
+                    oDTTable.fnFilter("",defaults.iIndex,true,false); 
+                    break;
+                case "_":
+                    oDTTable.fnFilter("^[0-9]",defaults.iIndex,true,false); 
+                    break;
+                default:
+                    oDTTable.fnFilter("^"+letter,defaults.iIndex,true,false); 
+            }
+            return false;
+        });
+
+	};
 
 	dt_abcdaire.prototype.getContainer = function() {
 		return this.$container.get( 0 );
-	}
-
+	};
 
 	/*
 	 * Register a new feature with DataTables
 	 */
 	if ( typeof $.fn.dataTable == "function" &&
-	     typeof $.fn.dataTableExt.fnVersionCheck == "function" &&
-	     $.fn.dataTableExt.fnVersionCheck('1.9.0') )
+        typeof $.fn.dataTableExt.fnVersionCheck == "function" &&
+        $.fn.dataTableExt.fnVersionCheck('1.9.0') )
 	{		
 		$.fn.dataTableExt.aoFeatures.push( 
-		{	        
-	        "fnInit": function( oDTSettings ) {
-	          var oWidgets = new dt_abcdaire( oDTSettings );
-	          return oWidgets.getContainer();
-	        },
-	        
+        {
+            "fnInit": function( oDTSettings ) {
+                var oWidgets = new dt_abcdaire( oDTSettings );
+                return oWidgets.getContainer();
+            },
 			"cFeature": "Z",
-	        "sFeature": "dt_abcdaire"
+            "sFeature": "dt_abcdaire"
 		} );
 	}
 	else
 	{
-		 alert( "Warning: Additionals Plugins requires DataTables 1.9.0 or greater - www.datatables.net/download");
+        alert( "Warning: Additionals Plugins requires DataTables 1.9.0 or greater - www.datatables.net/download");
 	}
 
 }(jQuery));
